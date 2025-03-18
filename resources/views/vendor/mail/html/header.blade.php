4d6ad284e7fbdb7c256194d41b9b8bfc8bf752fb
Arlx <aria.maulana.m@gmail.com>
2025-03-18 20:49:59 +0700

    fix: logo width

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,9 @@
 @props(['url'])
 <tr>
-    <td class="header">
+    <td class="header" style="text-align: center; padding: 20px 0;">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('assets/logo.png') }}" class="logo" alt="{{ config('app.name') }} Logo">
+            <img src="{{ asset('assets/logo.png') }}" style="width: 150px; height: auto; max-width: 100%;"
+                alt="{{ config('app.name') }} Logo">
         </a>
     </td>
 </tr>
